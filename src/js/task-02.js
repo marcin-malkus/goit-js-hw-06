e4001a09ed84b9a0d0e31803a10986cfd0fcc54d
Marcin Małkus <malkus.marcin@gmail.com>
2022-07-19 16:52:38 +0200

    task02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const ingredient of ingredients) {
+	const item = document.createElement("li");
+	item.textContent = ingredient;
+	item.classList.add("item");
+	list.append(item);
+}
