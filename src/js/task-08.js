6930e94a2a3de6673a4cfba2cb4693d68d93aa95
Marcin Małkus <malkus.marcin@gmail.com>
2022-07-20 18:38:53 +0200

    08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (!email.value || !password.value) {
+		alert("Wszystkie pola powinny być wypełnione!");
+	} else {
+		const data = {
+			email: email.value,
+			password: password.value,
+		};
+		console.log(data);
+		event.currentTarget.reset();
+	}
+});
